9c86c0628b4c94063870ba165a1ec1fcfbe0f7dc
feelsocold <feelsocoold@gmail.com>
2020-03-31 23:14:37 +0900

    bootstrap changed

--- src/main/java/com/bohan/manalive/web/controller/IndexController.java
+++ src/main/java/com/bohan/manalive/web/controller/IndexController.java
@@ -1,17 +1,48 @@
 package com.bohan.manalive.web.controller;
-
+import com.bohan.manalive.config.auth.LoginUser;
+import com.bohan.manalive.config.auth.dto.SessionUser;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
-    @GetMapping("/")
-    public String index() {
+    Logger logger = LoggerFactory.getLogger(getClass());
+    //private final HttpSession httpSession;
 
-        System.out.println("index()");
+    @GetMapping("/")
+    public String index(Model model, @LoginUser SessionUser user) {
+        logger.info("INDEX()");
+
+        //SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+            logger.info(user.getRole().getKey());
+            if(user.getRole().getKey().equals("ROLE_GUEST")){
+                return "redirect:/register";
+            }
+        }
 
         return "index";
     }
+    @GetMapping("/register")
+    public void register(Model model, @LoginUser SessionUser user) {
+        model.addAttribute("userName", user.getName());
+
+    }
+
+    @GetMapping("/main")
+    public String main() {
+
+
+        return "index2";
+    }
 
 }
+
